f5d04ec4a4086e896557cb55c1a9fe1b809960da
Dan Noble <dan@thehumangeo.com>
2012-08-24 16:50:32 -0400

    Added thrift support

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-RawES
+rawes
 =====
 
 About
 -----
-RawES is an elasticsearch driver for Python.  It provides a clean interface that is just a small wrapper on top of the native HTTP/Thrift interfaces to elasticsearch.
+rawes is an elasticsearch driver for Python.  It provides a clean interface that is just a small wrapper on top of the native interface to elasticsearch.
 
 Usage
 -----
@@ -17,6 +17,14 @@ Usage
 
 :: Lets you do date strings or Date() objects.
 
+
+Why rawes?
+----------
+elasticsearch's has a great native API, and I don't think there needs to be much abstraction on top of it.
+
+Features
+--------
+* Thrift support
 
 Run Unit Tests
 --------------
--- rawes/thrift_connection.py
+++ rawes/thrift_connection.py
@@ -1,9 +1,63 @@
+import json
+
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+
+from thrift_elasticsearch import Rest
+from thrift_elasticsearch.ttypes import Method, RestRequest
+
 class ThriftConnection(object):
     """docstring for ThriftConnection"""
     def __init__(self, host, port):
         super(ThriftConnection, self).__init__()
-        self.protocol = 'http'
+        self.protocol = 'thrift'
         self.host = host
         self.port = port
         self.url = '%s://%s:%s' % (self.protocol,self.host,self.port)
-        
+        tsocket = TSocket.TSocket(self.host, self.port)
+        transport = TTransport.TBufferedTransport(tsocket)
+        protocol = TBinaryProtocol.TBinaryProtocol(transport)
+        self.client = Rest.Client(protocol)
+        transport.open()
+
+    def get(self, path, **kwargs):
+        return self.request(Method.GET, path, **kwargs)
+    
+    def post(self, path, **kwargs):
+        return self.request(Method.POST, path, **kwargs)
+    
+    def put(self, path, **kwargs):
+        return self.request(Method.PUT, path, **kwargs)
+    
+    def delete(self, path, **kwargs):
+        return self.request(Method.DELETE, path, **kwargs)
+    
+    def head(self, path, **kwargs):
+        return self.request(Method.HEAD, path, **kwargs)
+    
+    def request(self, method, path, **kwargs):
+        thriftargs = {}
+        if kwargs.has_key('data'):
+            if type(kwargs['data']) == dict:
+                kwargs['data'] = json.dumps(kwargs['data'])
+            thriftargs['body'] = kwargs['data']
+
+        if kwargs.has_key('params'):
+            thriftargs['parameters'] = kwargs['params']
+
+        if kwargs.has_key('headers'):
+            thriftargs['headers'] = kwargs['headers']
+
+        request = RestRequest(method=method,uri=path,**thriftargs)
+        response = self.client.execute(request)
+
+        return ThriftConnection.decode(response)
+    
+    @classmethod
+    def decode(self, response):
+        if (response.body == ''):
+            return response.status < 300
+        return json.loads(response.body)
+            
