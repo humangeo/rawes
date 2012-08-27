b1634321bda78852f6b860529e5451e5856336e6
Dan Noble <dan@thehumangeo.com>
2012-08-27 17:42:47 -0400

    Updates to setup.py...

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from setuptools import setup
 setup(name='rawes',
     version='0.2',
     description='rawes elasticsearch driver',
+    long_description=open('README.md').read(),
     author='Dan Noble',
     author_email='@dwnoble',
     download_url='https://github.com/humangeo/rawes/tarball/master',
