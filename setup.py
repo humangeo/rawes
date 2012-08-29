e5de2e4ee66446c0202cd1b92c438b32f9fb2f9d
Dan Noble <dan@thehumangeo.com>
2012-08-29 11:38:53 -0400

    Added license to setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,10 @@ setup(name='rawes',
     author='Dan Noble',
     author_email='@dwnoble',
     download_url='https://github.com/humangeo/rawes/tarball/master',
+    license='Apache-2.0',
+    classifiers=[
+        'License :: OSI Approved :: Apache License 2.0 (Apache-2.0)'
+    ],
     url='https://github.com/humangeo/rawes',
     packages=['rawes', 'rawes.thrift_elasticsearch'],
 	install_requires=['requests==0.13.9'],
