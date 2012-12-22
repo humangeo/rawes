178597dcb23bf6199f1d7b9b4f35a1ff99b0088c
Dan Noble <dnoble@gmail.com>
2012-12-21 20:30:59 -0600

    bumped version for requests fix

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ classifiers = [
 ]
 
 setup(name='rawes',
-      version='0.3.2',
+      version='0.3.3',
       description='rawes elasticsearch driver',
       long_description="\n" + README + "\n\n" + CHANGES,
       author='Dan Noble',
